57d48cb3fb5845032fa4eea832305164b2f45996
Jacob Beard <jake@minnow.io>
2015-07-11 00:17:32 +0200

    Refactored to use the new common-expresscions library. Lots of other small changes, cleanup and documentation

--- README.md
+++ README.md
@@ -9,6 +9,46 @@ REST protocol.
 
 `npm install -g microexpresscion`
 
+## Usage
+
+`microexpresscion path/to/scxml`
+
+## API
+
+See `bin/www` for example of the JavaScript API.
+
+```javascript
+require('microexpresscion');
+microexpresscion.initExpress(
+
+microexpresscion.initExpress(pathToScxml,function(err, app){
+
+  if(err) throw err;
+
+  /**
+   * Create HTTP server.
+   */
+
+  var server = http.createServer(app);
+
+  /**
+   * Listen on provided port, on all network interfaces.
+   */
+
+  server.listen(port);
+  server.on('error', onError);
+  server.on('listening', onListening);
+
+  function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    console.log('Listening on ' + bind);
+  }
+});
+```
+
 ## Links
 
 * [State Machines as a Service: An SCXML Microservices Platform for the Internet of Things](http://scxmlworkshop.de/eics2015/submissions/State%20Machines%20as%20a%20Service.pdf)
--- index.js
+++ index.js
@@ -1,168 +1,111 @@
-#!/usr/bin/env node
 'use strict';
 
-var smaasApi = require('./app/api'),
-  cors = require('cors'),
-  scxml = require('scxml'),
-  fs = require('fs'),
-	validate = require('./app/validate-scxml').validateCreateScxmlRequest;
+var common = require('common-expresscions'),
+    scxml = require('scxml'),
+    uuid = require('uuid');
 
-//CLI: scxmld statechartName [instanceId]
-//scxmld.initApi({pathToModel : 'index.scxml', instanceId : null || instanceId})
+function initApi(model, scxmlString, modelName) {
+  var models = {};
+  var instances = {};
+  var instanceSubscriptions = {};
+  var api = {};
 
-function initExpress (opts, cb) {
-  opts = opts || {};
-  opts.port = opts.port || process.env.PORT || 8002;
+  function createNamedInstance(instanceId, res) {
+    var instance = new scxml.scion.Statechart(model, { sessionid: instanceId });
 
-  var express = require('express'),
-  path = require('path'),
-  logger = require('morgan'),
-  app = express();
+    instances[instanceId] = instance;
 
-  app.use(logger('dev'));
-  
-  // buffer the body
-  app.use(function(req, res, next) {
-    req.body = '';
-    req.on('data', function(data) {
-      return req.body += data;
-    });
-    return req.on('end', next);
-  });
-  
-  var websiteUrl = 'http://localhost:' + opts.port;
-  
-  if (process.env.WEBSITE_URL) {
-    websiteUrl = process.env.WEBSITE_URL;
-  } else {
-    console.log('Missing "WEBSITE_URL" variable.');
-  }
+    res.setHeader('Location', instanceId);
 
-  app.use(cors({
-    origin: websiteUrl,
-    exposedHeaders: ['WWW-Authenticate', 'Location', 'X-Configuration']
-  }));
-
-  app.set('views', path.join(__dirname, './views'));
-  app.engine('html', require('ejs').renderFile);
-  app.use(express.static(path.join(__dirname, './node_modules/expresscion-portal/app')));
-  app.use(express.static(path.join(__dirname, './public')));
-
-  opts.app = app;
-
-  initApi(opts, cb);
-}
-
-function initApi(opts, cb){
-  opts = opts || {};
-  opts.port = opts.port || process.env.PORT || 8002;
-  opts.basePath = opts.basePath || '/api/v3';
-
-  if(process.env.SIMULATION_PROVIDER){
-    opts.simulationProvider = require(process.env.SIMULATION_PROVIDER);
-  }
-
-  opts.middlewares = opts.middlewares || [];
-
-  process.env.SEND_URL = process.env.SEND_URL || ('http://localhost:' + opts.port + opts.basePath + '/');
-
-  if(!opts.app) {
-    return cb(new Error('Missing express app'));
-  }
-
-  var totalRequestCount = 0;
-  opts.app.use(function (req, res, next) {
-    //We are providing an id to each request and response
-    //So we can unregister "_changes" listeners on stateless servers
-    req.uniqueId = res.uniqueId = totalRequestCount++;
-
-    next();
-  });
-
-  fs.readFile(opts.pathToModel, 'utf8', function (err, scxmlString) {
-    if(err) throw err;
-
-    validate(scxmlString, function(scxmlSchemaErrors) {   
-      if(scxmlSchemaErrors) throw scxmlSchemaErrors;
-
-      scxml.pathToModel(opts.pathToModel, function(err, model){
-
-        var modelName = process.env.APP_NAME || model.meta.name;
-
-        // Initialize the api
-        //var simulation = opts.simulationProvider(db, model, modelName );
-
-        var api = smaasApi( model, scxmlString, modelName );
-
-        var smaasJSON = require('smaas-swagger-spec');
-
-        smaasJSON.host = process.env.HOST || ('localhost' + ':' + opts.port);
-        smaasJSON.basePath = opts.basePath;
-
-        opts.app.get('/smaas.json', function (req, res) {
-          res.status(200).send(smaasJSON);
-        });
-
-        opts.app.get(smaasJSON.basePath + '/:InstanceId/_viz', api.instanceViz);
-        opts.app.get(smaasJSON.basePath + '/_viz', api.statechartViz);
-
-        function methodNotImplementedMiddleware(req, res){
-          return res.send(501, { message: 'Not implemented' });
-        }
-
-        Object.keys(smaasJSON.paths).forEach(function(endpointPath){
-          var endpoint = smaasJSON.paths[endpointPath];
-          var actualPath = smaasJSON.basePath + endpointPath.replace(/{/g, ':').replace(/}/g, '');
-
-          Object.keys(endpoint).forEach(function(methodName){
-            var method = endpoint[methodName];
-
-            var handler = api[method.operationId] || methodNotImplementedMiddleware;
-            switch(methodName) {
-              case 'get': {
-                opts.app.get(actualPath, opts.middlewares, handler);
-                break;
-              }
-              case 'post': {
-                opts.app.post(actualPath, opts.middlewares, handler);
-                break;
-              }
-              case 'put': {
-                opts.app.put(actualPath, opts.middlewares, handler);
-                break;
-              }
-              case 'delete': {
-                opts.app.delete(actualPath, opts.middlewares, handler);
-                break;
-              }
-              default:{
-                console.log('Unsupported method name:', methodName);
-              }
-            }
-          });
-        });
-
-        cb(null, opts);
-      });    
-    });
-  });
-}
-
-if(require.main === module) {
-  var opts = {};
-  opts.pathToModel = process.argv[2] || 'main.scxml';
-
-  initExpress(opts, function (err, opts) {
-    if(err) throw err;
-
-    console.log('Starting server on port:', opts.port);
-    opts.app.listen(opts.port, function () {
-      console.log('Server started');
-    });
-  });
-} else {
-  module.exports = {
-    initApi: initApi,
-    initExpress: initExpress
+    res.status(201).send({ name: 'success.create.instance', data: { id: instanceId }});
   };
-}
+
+  api.getStatechartDefinition = function(req, res){
+    res.type('application/scxml+xml').status(200).send(scxmlString);
+  };
+
+  api.createInstance = function(req, res) {
+    var instanceId = uuid.v1();
+    createNamedInstance(instanceId, res);
+  };
+
+  api.createNamedInstance = function(req, res) {
+    var instanceId = req.params.InstanceId;
+    if(instances[instanceId]){
+      return res.status(409).send({ name: 'error.creating.instance', data: { message: 'InstanceId is already associated with an instance' }});
+    }
+
+    createNamedInstance(instanceId, res);
+  };
+
+  api.getInstances = function(req, res) {
+    res.send({ name: 'success.get.instances', data: {instances : Object.keys(instances)}});
+  };
+
+  api.getInstance = function(req, res){
+    getInstance(req, res, function(instanceId, instance){
+      res.send({ name: 'success.get.instance', data: { instance: { snapshot: instance.getSnapshot() }}});
+    });
+  };
+
+  api.sendEvent = function(req, res) {
+    getInstance(req, res, function(instanceId, instance){
+      var event = req.body;
+
+      try {
+        if(event.name === 'system.start') {
+          instance.start();
+        } else {
+          instance.gen(event);
+        }
+      } catch(e){
+        return res.status(500).send({ name: 'error.sending.event', data: e.message});
+      }
+
+      var snapshot = instance.getSnapshot();
+      res.setHeader('X-Configuration',JSON.stringify(snapshot[0]));
+      //TODO: handle custom data
+      return res.send({ name: 'success.event.sent', data: { snapshot: snapshot }});
+    });
+  };
+
+  function getInstance(req, res, done){
+    var instanceId = req.params.InstanceId;
+    var instance = instances[instanceId];
+    if(instance){
+      done(instanceId, instance);
+    } else {
+      res.status(404).send({'name':'error.instance.not.found'});
+    }
+  }
+
+  api.deleteInstance = function(req, res){
+    getInstance(req, res, function(instanceId, instance){
+      delete instances[instanceId];
+      res.send({ name: 'success.deleting.instance', data: { message: 'Instance deleted successfully.' }});
+    });
+  };
+
+  api.getInstanceChanges = function(req, res){
+    getInstance(req, res, function(instanceId, instance){
+      common.sse.initStream(req, res, function(){});
+
+      instanceSubscriptions[instanceId] = instanceSubscriptions[instanceId] || [];
+
+      instanceSubscriptions[instanceId].push(res);
+
+      instance.on('*',function(name, data){
+        res.write('event: ' + name + '\n');
+        res.write('data: ' + data + '\n\n');
+      });
+    });
+  };
+
+  api.getEventLog = function (req, res) {
+    res.status(501).send({'name':'Not implemented'});
+  };
+
+  return api;
+};
+
+module.exports.initExpress = common.initExpress.bind(this, initApi);
